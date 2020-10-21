8aaf38f3801c2fdb798f466164761f5a5fb1d4c9
trashbirdecology <jessicaleighburnett@gmail.com>
2020-10-21 08:47:50 -0600

    Updates

--- R/download_plans.R
+++ R/download_plans.R
@@ -23,7 +23,8 @@ download_plans <-
               countries = NULL,
               states = NULL,
               inverse.states = NULL,
-              dl.mode="wb") {
+              dl.mode = "wb",
+              overwrite.plans = FALSE) {
         # create the dl directory if it does not exist
         if (!dir.exists(dir)) {
             dir.create(dir)
@@ -33,12 +34,12 @@ download_plans <-
         data("plan_urls")
 
 
-# SUBSET the lookup table by state, inverstate, and/or region
+        # SUBSET the lookup table by state, inverstate, and/or region
         # states = c("FL") #for testing
         if (!is.null(states)) {
             plan_urls <- plan_urls %>%
                 dplyr::filter(state %in% states)
-            }
+        }
         # subset download query by state, inverstate, and/or region
         if (!is.null(inverse.states)) {
             plan_urls.subset <- plan_urls %>%
@@ -46,28 +47,55 @@ download_plans <-
         }
 
         # keep countries
-        if (!is.null(country)) {
+        if (!is.null(countries)) {
             plan_urls <- plan_urls %>%
                 dplyr::filter(country %in% countries)
         }
 
-# DOWNLOAD the remaining from urlsin lookup table
-for(i in 1:nrow(plan_urls)){
-   temp <- plan_urls[i,]
-   if(is.na(temp$url_dl))next()
-   if(is.na(year.ind)){year.ind="NaN"}
+        # DOWNLOAD the remaining from urlsin lookup table
+        fn.out <- NULL
+        for (i in 1:nrow(plan_urls)) {
+            fn <- NULL
+            temp <- plan_urls[i, ]
+            if (is.na(temp$url_dl))
+                next()
+            if (is.na(temp$year_start)) {
+                year.ind = "NaN"
+            }
 
-   fn <- paste0(temp$year_start, "_",temp$country,"_", temp$state, "_", temp$plan_type,".", temp$filetype)
+            fn <-
+                paste0(
+                    temp$year_start,
+                    "_",
+                    temp$country,
+                    "_",
+                    temp$state,
+                    "_",
+                    temp$plan_type,
+                    ".",
+                    temp$filetype
+                )
 
-   if (!overwrite.plans &
-       fn %in% list.files(dir)) {
-       message(fn,
-               "already exists and overwrite.plans=FALSE. not downloading this file.")
-   }else(download.file(url = temp$url_dl,
-                                         destfile = paste(dir,fn),
-                                         mode = dl.mode))
-}
+            if (!overwrite.plans &
+                fn %in% list.files(dir)) {
+                message(fn,
+                        "already exists and overwrite.plans=FALSE. not downloading this file.")
+            } else
+                (download.file(
+                    url = temp$url_dl,
+                    destfile = paste(dir, fn),
+                    mode = dl.mode,
+                    quiet=TRUE
+                ))
+            fn.out <- c(fn.out, fn)
+        }
 
-message("for a list of downloaded files, please call `list.files(", paste(dir),")`", " or browse this directory")
+        message(
+            "for a list of downloaded files, please call `list.files(",
+            paste(dir),
+            ")`",
+            " or browse this directory"
+        )
 
+return(fn.out)
     }# end download_plans function
